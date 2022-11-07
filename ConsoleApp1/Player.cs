f6094455afd0984cc3ac7c2b8cec4189261767bd
Samuel Kuta <kuta@SPSEJECNANET.CZ>
2022-11-07 08:35:46 +0100

    first commit

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Player
+    {
+        string name = "CervenaKarkulka";
+        pole field;
+
+        public Player()
+        {
+            this.field = new pole("lvl",4);
+        }
+
+
+
+        public void moveLeft()
+        {
+            
+        }
+
+        public void moveRight()
+        {
+
+        }
+
+        public void moveUp()
+        {
+
+        }
+
+        public void moveDown()
+        {
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/pole.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class pole
+    {
+        
+        public string lvlName;
+        public int size;
+        public fieldItem start;
+        public fieldItem end;
+
+
+        public pole(string lvlName, int size)
+        {
+            this.lvlName = lvlName;
+            this.size = size;
+        }
+
+
+        public void generateSqare()
+        {
+            fieldItem lastMade;
+
+            fieldItem? north;
+            fieldItem? south;
+            fieldItem? east;
+            fieldItem? west;
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; i++)
+                {
+                   if(i == 0 && j == 0)
+                    {
+                        north = null;
+
+                    } 
+                }
+            }
+        }
+
+
+        
+
+        internal class fieldItem
+        {
+
+
+            public string name;
+            public fieldItem north;
+            public fieldItem south;
+            public fieldItem west;
+            public fieldItem east;
+
+            public fieldItem(string name, fieldItem north, fieldItem south, fieldItem west, fieldItem east)
+            {
+                this.name = name;
+                this.north = north;
+                this.south = south;
+                this.west = west;
+                this.east = east;
+            }   
+        }
+    }
+}   
